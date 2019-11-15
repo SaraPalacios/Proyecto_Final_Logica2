b65bb17870aa1f226d19599a8a713c13051aa1c9
Sara <saris_pal@hotmail.com>
2019-11-14 23:29:04 -0500

    mas dpll

--- Codigo probablemente util/dpll.py
+++ Codigo probablemente util/dpll.py
@@ -56,10 +56,39 @@ def DPLL(S, I):
         return "insatisfacible", {}
     if len(sol[0])==0:
         return "Satisfacible", sol[1]
+    l = sol[0][0][0]
+    sp = S.copy()
+    Ip = I.copy()
+    if l != '-':
+        for elem in sp:
+            if complem(l) in elem:
+                res = elem.replace(complem(l),'')
+                sp.remove(elem)
+                sp.append(res)
+        for el in sp:
+            if l in el:
+                sp.remove(el)
+        Ip[l]=1
+        Ip[complem(l)] = 0
+    else: 
+        for elem in sp:
+            if l in elem:
+                res = elem.replace(l,'')
+                sp.remove(elem)
+                sp.append(res)
+        for el in S:
+            if l[1] in el:
+                sp.remove(el)
+       
+        Ip[l]=0
+        Ip[complem(l)]=1
+    sol2 = DPLL(sp,Ip)
+    if sol2[0]=="Satisfacible":
+        return "Satidsfacible", Ip
+   
+    
 
-
-
-s = ["p", "-pq-r", "q"]
+s = ["p", "-pq", "-qrs"]
 
 
 print(DPLL(s,{}))
