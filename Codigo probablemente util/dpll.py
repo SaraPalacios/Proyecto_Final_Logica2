709edec0451325a6e92530a7e5c7041108ae6568
Sara <saris_pal@hotmail.com>
2019-11-15 00:28:07 -0500

    Ultima parte dpll

--- Codigo probablemente util/dpll.py
+++ Codigo probablemente util/dpll.py
@@ -38,11 +38,22 @@ def remove_clausuni(S, cuni):
             S.remove(el)
     return S, cuni
 
+def remv_lite(S, lit):
+    for elem in S:
+        if complem(lit) in elem:
+            res = elem.replace(complem(lit),'')
+            S.remove(elem)
+            S.append(res)
+    for el in S:
+        if lit in el:
+            S.remove(el)
+
+
+
 def unit_prop(S, I):
     while tieneclausulaunit(S)[0] and voidclaus(S) == False:
         if is_neg(tieneclausulaunit(S)[1]):
             I[tieneclausulaunit(S)[1]] = 0
-
         else:
             I[tieneclausulaunit(S)[1]] = 1 
     
@@ -60,23 +71,17 @@ def DPLL(S, I):
     sp = S.copy()
     Ip = I.copy()
     if l != '-':
-        for elem in sp:
-            if complem(l) in elem:
-                res = elem.replace(complem(l),'')
-                sp.remove(elem)
-                sp.append(res)
-        for el in sp:
-            if l in el:
-                sp.remove(el)
+        remv_lite(sp,l)
         Ip[l]=1
         Ip[complem(l)] = 0
     else: 
+        l = '-'+sol[0][0][1]
         for elem in sp:
             if l in elem:
                 res = elem.replace(l,'')
                 sp.remove(elem)
                 sp.append(res)
-        for el in S:
+        for el in sp:
             if l[1] in el:
                 sp.remove(el)
        
@@ -85,10 +90,38 @@ def DPLL(S, I):
     sol2 = DPLL(sp,Ip)
     if sol2[0]=="Satisfacible":
         return "Satidsfacible", Ip
-   
-    
+    else:
+        s3 = sol[0].copy()
+        I3 = I.copy()
+        if l!='-':
+            for el in s3:
+                if l in el:
+                    res = el.replace(l,'')
+                    s3.remove(el)
+                    s3.append(res)
+            for el in s3:
+                if complem(l) in el:
+                    s3.remove(el)
+            I3[l]=0
+            I3[complem(l)]=1
+        else:
+            l =sol[0][0][1]
+            for el in s3:
+                if '-'+l in el:
+                    res = el.replace('-'+l,'')
+                    s3.remove(el)
+                    s3.append(res)
+            for el in s3:
+                if complem('-'+l) in el:
+                    s3.remove(el)
 
-s = ["p", "-pq", "-qrs"]
+            I3[l]= 1
+            I3[complem(l)] = 0
+        sol3 = DPLL(s3,I3)
+        return sol3
+
+
+s = ["p-qr", "-pq-r", "-p-qr", "-p-q-r"]
 
 
 print(DPLL(s,{}))
